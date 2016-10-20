ddd00bebfec1c728cb85bc951dc40c4f36c2e587
George Higbie <gehigbie@gmail.com>
2016-10-20 12:56:48 -0400

    added splash page

---
+++ app/src/main/java/com/geogehigbie/digitalleashchild/SplashPage.java
@@ -0,0 +1,24 @@
+package com.geogehigbie.digitalleashchild;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by georgehigbie on 10/20/16.
+ */
+
+public class SplashPage extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+
+
+    }
+}
