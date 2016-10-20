95329cad68fa3447dbc8c9bd6dee46301781373b
George Higbie <gehigbie@gmail.com>
2016-10-20 13:34:13 -0400

    created fragmens and added to view

--- app/src/main/java/com/geogehigbie/digitalleashchild/MainActivity.java
+++ app/src/main/java/com/geogehigbie/digitalleashchild/MainActivity.java
@@ -2,13 +2,34 @@ package com.geogehigbie.digitalleashchild;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.Window;
+
+import layout.FragmentDataChild;
 
 public class MainActivity extends FragmentActivity {
+
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+
+        loadFirstFragment();
+
+    }
+
+    public void loadFirstFragment(){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragmentContainer, new FragmentDataChild());
+
     }
 
 
